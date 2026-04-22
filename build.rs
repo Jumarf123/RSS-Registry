3e1a18571a2830a9a4a8e95db061de1e5680dff9
Jumarf123 <simonenkob819@gmail.com>
2026-04-22 15:13:04 +0800

    Add files via upload

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
-use std::io;
+use std::{env, io};
 
 fn main() -> io::Result<()> {
+    println!("cargo:rerun-if-env-changed=RSS_REGISTRY_SKIP_ADMIN_MANIFEST");
+
     // Build Windows resources: icon + manifest with requireAdministrator.
-    if cfg!(target_os = "windows") {
+    if cfg!(target_os = "windows") && env::var_os("RSS_REGISTRY_SKIP_ADMIN_MANIFEST").is_none() {
         let mut res = winres::WindowsResource::new();
         res.set_icon("rss.ico");
         res.set_manifest_file("app.manifest");
